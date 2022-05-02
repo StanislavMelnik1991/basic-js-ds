38e126c90c0d65d780b4fa62b3dacde2e3c32e1b
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-05-02 15:46:19 +0300

    feat: task binary-search start

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,70 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
-
+  constructor(data) {
+    this.rootValue = data ? new Node(data) : null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootValue
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    const insertNode = (node, newNode)=> {
+      if (newNode.data < node.data) {
+          if (node.left === null) {
+              node.left = newNode;
+          } else {
+              insertNode(node.left, newNode);
+          }
+      } else {
+          if (node.right === null) {
+              node.right = newNode;
+          } else {
+              insertNode(node.right, newNode);
+          }
+      }
+    }
+    if (this.rootValue === null) {
+        this.rootValue = newNode;
+    } else {
+        insertNode(this.rootValue, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(findData) {
+    
+    const findNode = (node, findData)=> {
+      if (node === null) {
+        return null
+      }
+      if (findData === node.data) {
+        return node
+      }else{
+        if (findData < node.data) {
+            return findNode(node.left, findData);
+          }
+        else {
+            return findNode(node.right, findData);
+        }
+      }
+      
+    }
+    
+      return findNode(this.rootValue, findData);
+ 
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const tmp = this.find(data)
+    return (tmp === null ? false: true);
   }
 
   remove(/* data */) {
